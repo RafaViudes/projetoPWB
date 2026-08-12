68c9052062047f4050f725582a9b8a0e2bc7d5c4
guilhermeSR1 <warrrion971@gmail.com>
2026-08-12 11:04:30 -0300

    FormularioLogin

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 2</title>
-</head>
-<body>
-        <form action="cadastro.php" method="post"> 
-    <label>Nome</label>
-    <input type="text" name="nome" id="nome">
-    <input type="email" name="email" id="email">
-    <input type="password" name="senha" id="senha">
-    <input type="submit" value="Cadastrar">
-</form>
-</body>
-</html>
+<?php include "cabecalho.php"; ?>
+
+<h1>Página inicial</h1>
+
+<?php include "rodape.php"; ?>
